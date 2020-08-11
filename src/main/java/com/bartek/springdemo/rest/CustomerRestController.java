334c4640e2316a3a4e145b1751a6f646fd353188
Bartosz Walczak <bl4kee@wp.pl>
2020-08-11 07:06:35 +0200

    Added option for remove customer

--- src/main/java/com/bartek/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/bartek/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.bartek.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,13 @@ public class CustomerRestController {
 	public Customer updateCustomer(@RequestBody Customer theCustomer) {
 		customerService.saveCustomer(theCustomer);
 		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		if(tempCustomer == null) {throw new CustomerNotFoundException("Customer id not found - " + customerId);}
+		customerService.deleteCustomer(customerId);
+		return "Deleted customer id - " + customerId;
 	}
 }
